90e3a2c0d104296de6c23ed2bd739685220f99c0
Roi-Feldenkreis <125310115+Roi-Feldenkreis@users.noreply.github.com>
2025-02-12 00:05:00 +0200

    Update mfFDs.py
    cleanig the file

--- RANSAC/mfFDs.py
+++ RANSAC/mfFDs.py
@@ -2,26 +2,17 @@ import numpy as np
 
 
 def mfFDs(F, u):
-    # MATLAB: Fu1 = F*u(4:6,:)
+    
     Fu1 = F @ u[3:6, :]
-
-    # MATLAB: Fu2 = (F'*u(1:3,:)).^2
     Fu2 = (F.T @ u[0:3, :]) ** 2
-
-    # MATLAB: Fu1pow = Fu1.^2
     Fu1pow = Fu1 ** 2
-
-    # MATLAB: denom = Fu1pow(1,:)+Fu1pow(2,:)+Fu2(1,:)+Fu2(2,:)
     denom = Fu1pow[0, :] + Fu1pow[1, :] + Fu2[0, :] + Fu2[1, :]
-
-    # MATLAB: errvec = zeros(1,size(u,2))
     errvec = np.zeros(u.shape[1])
 
     for i in range(u.shape[1]):
         # MATLAB: xFx = u(1:3,i)'*Fu1(:,i)
         xFx = u[0:3, i].T @ Fu1[:, i]
 
-        # MATLAB: errvec(i) = xFx^2/denom(i)
         errvec[i] = xFx ** 2 / denom[i]
 
     err = errvec
@@ -29,7 +20,7 @@ def mfFDs(F, u):
 
 
 # Example usage
-F = np.random.rand(3, 3)  # Example F matrix
-u = np.random.rand(6, 10)  # Example u matrix with 10 correspondences
-err = mfFDs(F, u)
-print(err)
+# F = np.random.rand(3, 3)  # Example F matrix
+# u = np.random.rand(6, 10)  # Example u matrix with 10 correspondences
+# err = mfFDs(F, u)
+# print(err)
