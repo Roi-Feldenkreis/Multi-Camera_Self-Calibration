138616877566060a8213620708617a1019aea5a5
Roi-Feldenkreis <125310115+Roi-Feldenkreis@users.noreply.github.com>
2025-02-21 10:02:20 +0200

    Update mfFDs.py
    adding explanation to the function

--- RANSAC/mfFDs.py
+++ RANSAC/mfFDs.py
@@ -2,7 +2,21 @@ import numpy as np
 
 
 def mfFDs(F, u):
-    
+    """
+    Computes an error metric based on the epipolar constraint.
+
+    This function evaluates the error associated with a fundamental matrix F and a set of point correspondences u.
+    The error is computed based on a normalized epipolar constraint formulation.
+
+    Parameters:
+    F (numpy.ndarray): A 3x3 fundamental matrix.
+    u (numpy.ndarray): A 6xN matrix, where:
+        - The first three rows (0:3) correspond to homogeneous coordinates of image points.
+        - The last three rows (3:6) correspond to their corresponding points in another image.
+
+    Returns:
+    numpy.ndarray: A 1D array of length N containing the computed error values for each correspondence.
+    """
     Fu1 = F @ u[3:6, :]
     Fu2 = (F.T @ u[0:3, :]) ** 2
     Fu1pow = Fu1 ** 2
@@ -10,9 +24,7 @@ def mfFDs(F, u):
     errvec = np.zeros(u.shape[1])
 
     for i in range(u.shape[1]):
-        # MATLAB: xFx = u(1:3,i)'*Fu1(:,i)
         xFx = u[0:3, i].T @ Fu1[:, i]
-
         errvec[i] = xFx ** 2 / denom[i]
 
     err = errvec
