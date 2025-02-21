dc2de0d59e613a747b65d38f8c5411eabf816fa6
Roi-Feldenkreis <125310115+Roi-Feldenkreis@users.noreply.github.com>
2025-02-21 10:11:41 +0200

    Update p2e.py
    adding a explanation to the function

--- RANSAC/p2e.py
+++ RANSAC/p2e.py
@@ -1,10 +1,21 @@
 import numpy as np
 
 def p2e(u):
+    """
+    Converts homogeneous coordinates to Euclidean coordinates.
+
+    Parameters:
+    -----------
+    u : numpy.ndarray a 3xN array where each column represents a point in homogeneous coordinates.
+
+    Returns:
+    --------
+    e : numpy.ndarray a 2xN array where each column represents the point in Euclidean coordinates.
+    """
     e = u[:2, :] / u[2, :]
     return e
 
 # Example usage
-u = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-e = p2e(u)
-print(e)
+# u = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# e = p2e(u)
+# print(e)
