2d44c2ee4df2f9f5a00998ec0d268e88733f4853
Roi-Feldenkreis <125310115+Roi-Feldenkreis@users.noreply.github.com>
2025-02-21 10:59:55 +0200

    Update nsamples.py
    Adding an explanation to the function

--- RANSAC/nsamples.py
+++ RANSAC/nsamples.py
@@ -1,7 +1,30 @@
 import numpy as np
 
 
-def nsamples(ni, ptNum, pf, conf):
+def nsamples(ni, ptNum, pf, conf=):
+     """
+    Computes the required number of random samples to achieve a desired confidence level
+    in robust estimation.
+
+    Parameters:
+    -----------
+    ni : (nt) The number of inliers in the data.
+    ptNum : (int) The total number of points (both inliers and outliers).
+    pf : (int) The number of points needed to estimate the model.
+    conf : (float) The desired confidence level (typically between 0 and 1).
+
+    Returns:
+    --------
+    SampleCnt : (float) The estimated number of random samples needed to achieve the given confidence level.
+
+    Explanation:  
+    ------------
+    This function calculates the probability `q` that a randomly selected sample consists entirely of inliers,
+    Based on `q`, the function determines the minimum number of samples required.
+    This calculation is commonly used in RANSAC to determine how many iterations 
+    are needed to find a valid model with high confidence.
+    """
+
     q = np.prod([(ni - pf + 1 + i) / (ptNum - pf + 1 + i) for i in range(pf)])
 
     if (1 - q) < np.finfo(float).eps:
@@ -16,6 +39,7 @@ def nsamples(ni, ptNum, pf, conf):
 
 
 # Example usage
+"""
 ni = 10  # Number of inliers
 ptNum = 100  # Total number of points
 pf = 8  # Number of points needed for the model
@@ -23,3 +47,4 @@ conf = 0.99  # Desired confidence level
 
 sample_count = nsamples(ni, ptNum, pf, conf)
 print(sample_count)
+"""
